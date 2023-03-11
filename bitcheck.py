f88eb29a699e802c3875a4a4805c4a3f42ac147a
Kn0ax <42520461+Kn0ax@users.noreply.github.com>
2023-03-11 23:47:33 +0100

    Update bitcheck.py for bit button availability check

--- bitcheck.py
+++ bitcheck.py
@@ -3,7 +3,6 @@ from gql.transport.aiohttp import AIOHTTPTransport
 import requests
 import json
 
-# Define the query as a gql object
 
 
 #read channel data from file
@@ -18,32 +17,6 @@ transport = AIOHTTPTransport(url="https://gql.twitch.tv/gql/", headers=header)
 client = Client(transport=transport, fetch_schema_from_transport=False)
 
 
-with open("banned.txt", "w") as bannedfile, open("ispartner.txt", "w") as partner_outfile:
-    for channel in channels:
-        query = gql(f'''
-        query {{
-          userResultByLogin(login: "{channel}") {{
-            ...on UserDoesNotExist {{
-                key
-                reason
-            }}
-          }}
-        }}
-        ''')
-        try:
-            result = client.execute(query)
-        except Exception as e:
-            print(f"Error Occured while fetching result for channel : {channel}")
-            print(e)
-            continue
-        userResultByLogin = result.get('userResultByLogin', {})
-        if 'reason' in userResultByLogin:
-            bannedfile.write(f'{channel} : {userResultByLogin["reason"]}\n')  # write to the output file
-        else:
-            partner_outfile.write(f"{channel}\n") #write to ispartner.txt
-            print(f"No Reason found for channel {channel}")
-
-
 with open("ispartner.txt", "w") as partner_outfile, open("still_partner.txt", "w") as still_partner:
     for channel in channels:
         query = gql(f'''
@@ -53,6 +26,9 @@ with open("ispartner.txt", "w") as partner_outfile, open("still_partner.txt", "w
               isAffiliate
               isPartner
             }}
+            cheer {{
+              id
+            }}            
           }}
         }}
         ''')
@@ -68,9 +44,16 @@ with open("ispartner.txt", "w") as partner_outfile, open("still_partner.txt", "w
             continue
         roles = user.get("roles", {})
         if roles.get('isPartner') == True:
-            still_partner.write(f"{channel} is a Partner\n")
+            if user.get('cheer') is None:
+                still_partner.write(f"{channel} is a Partner but cheer value is None\n")
+            else:
+                still_partner.write(f"{channel} is a Partner\n")
         elif roles.get('isAffiliate') == True:
-            still_partner.write(f"{channel} is an Affiliate\n")
+            if user.get('cheer') is None:
+                still_partner.write(f"{channel} is an Affiliate but cheer value is None\n")
+            else:
+                still_partner.write(f"{channel} is an Affiliate\n")
         else:
             partner_outfile.write(f"{channel} is not a Partner or Affiliate\n")
-##loky aç abone olsana <3 btw: ytd: bit long to the moon
+#loky aç abone olsana <3 
+# btw: ytd: bit long to the moon
